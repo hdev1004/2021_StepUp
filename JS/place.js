46f6f48fdb186078ec617651a9d918b3150f4b8e
name <god@god.com>
2021-05-14 13:25:53 +0900

    commit

---
+++ JS/place.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    dogShow();
+  }
+  
+  function dogShow(){
+  
+    setTimeout(function(){
+        document.getElementById('dog').src = '/Img/dog_stop.png';
+    },9500);
+  
+  }
